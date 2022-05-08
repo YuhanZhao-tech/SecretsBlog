404609f325f164642fbbbe1b1d86a1c64ad12e7b
YuhanZhao-tech <aidenzhao@hotmail.com>
2022-05-08 11:08:45 -0400

    Added session with passport

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res) {
 	res.render("home");
 });
@@ -42,15 +46,64 @@ app.get("/login", function (req, res) {
 	});
 });
 
+app.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/");
+})
+
+app.get("/secrets", function(req, res) {
+	res.set(
+		'Cache-Control',
+		'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check-0'
+	);
+	if (req.isAuthenticated()) res.render("secrets");
+	else {
+		res.redirect("/login");
+	}
+})
+
 app.post("/register", function (req, res) {
 
-    
+	// User.register is from passport local mongoose
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+			res.redirect("/register");
+		}
+		else {
+			passport.authenticate("local")(req, res, function() {
+				res.redirect("/secrets");
+			})
+		}
+	})
 
 });
 
-app.post("/login", function (req, res) {
-	
-});
+app.post(
+	"/login", 
+	passport.authenticate("local", {
+		successRedirect: "/secrets",
+		faiureRedirect: "/login"
+	})
+	// function (req, res) {
+	// 	const user = new User({
+	// 		username: req.body.username,
+	// 		password: req.body.password
+	// 	})
+
+	// 	req.login(user, function(err) {
+	// 		if (err) {
+	// 			console.log(err);
+	// 		} else if (user) {
+	// 			passport.authenticate("local")(req, res, function() {
+	// 				res.redirect("/secrets");
+	// 			})
+	// 		} else {
+	// 			res.redirect("/login");
+	// 		}
+	// 	})
+	// }
+);
 
 app.listen(3000 || process.env.PORT, function () {
 	console.log("Server started successfully");
